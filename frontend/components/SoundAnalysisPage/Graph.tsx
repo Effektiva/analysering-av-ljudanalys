094cb925c8c2efc1033507f31d7e0932c31fb908
Max Wilén <maxwi734@student.liu.se>
2023-04-25 16:41:03 +0200

    48: probs...

--- frontend/components/SoundAnalysisPage/Graph.tsx
+++ frontend/components/SoundAnalysisPage/Graph.tsx
@@ -1,14 +1,55 @@
+import { useRef } from 'react';
+import { AreaChart } from '@toast-ui/react-chart';
+
 const STYLE_NAMESPACE = "graph__";
 enum Style {
   Container = STYLE_NAMESPACE + "container",
 }
 
+
+const data = {
+  categories: [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ],
+  series: [
+    {
+      name: 'Seoul',
+      data: [20, 40, 25, 50, 15, 45, 33, 34, 20, 30, 22, 13],
+    },
+    {
+      name: 'Sydney',
+      data: [5, 30, 21, 18, 59, 50, 28, 33, 7, 20, 10, 30],
+    },
+    {
+      name: 'Moscow',
+      data: [30, 5, 18, 21, 33, 41, 29, 15, 30, 10, 33, 5],
+    },
+  ],
+};
+const options = {
+  chart: { title: 'Average Temperature', width: 900, height: 400 },
+  xAxis: { pointOnColumn: false, title: { text: 'Month' } },
+  yAxis: { title: 'Temperature (Celsius)' },
+};
+
+
+
 const Graph = () => {
   return (
-    <div className={Style.Container}>
-      Graf
-    </div>
+    <AreaChart ref={useRef(null)} data={data} options={options} />
   );
 }
 
 export default Graph;
+
