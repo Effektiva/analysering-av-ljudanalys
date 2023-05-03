d1e626cd2a80b6a969d7640acf22158c49619c43
Zackarias Wadströmer <zacwa923@student.liu.se>
2023-05-03 09:17:52 +0000

    Visual fixes to left-menu

--- frontend/components/Popup.tsx
+++ frontend/components/Popup.tsx
@@ -2,6 +2,7 @@ import { LegacyRef, ReactElement } from "react";
 
 type Props = {
   component: ReactElement,
+  title: string,
   reference: LegacyRef<HTMLDivElement>,
   closeHandler: Function,
 }
@@ -11,8 +12,13 @@ const Popup = (props: Props) => {
     <div className="popupOverlay">
       <div ref={props.reference} className="popupContainer">
         <div className="popup">
-          <div onClick={() => props.closeHandler()} className="popupCloseButton">X</div>
-          { props.component }
+          <div className="popupTop">
+            <div className="popupTitle"> {props.title} </div>
+            <div onClick={() => props.closeHandler()} className="popupCloseButton">X</div>
+          </div>
+          <div className="popupContent">
+            { props.component }
+          </div>
         </div>
       </div>
     </div>
