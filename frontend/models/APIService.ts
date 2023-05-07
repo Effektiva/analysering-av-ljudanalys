f7cb82d1a8fdf0a07eb3f1b08b91e26926a7dca8
Rasmus Wallin <raswa151@student.liu.se>
2023-05-07 11:44:43 +0000

    Resolve "funktionalitet för att sätta status på ljudkedja"

--- frontend/models/APIService.ts
+++ frontend/models/APIService.ts
@@ -139,6 +139,18 @@ class APIService {
     return result;
   }
 
+  static setSoundchainState = async (investigationId: number, soundchainId: number, state: number) => {
+    log.debug("Change state of", soundchainId, "to", state);
+    await axios.put(this.apiURL + "/investigations/" + investigationId +
+                                  "/soundchains/",
+                    {"id": soundchainId, "state": state})
+               .then((response: any) => {
+        if (response.status !== 200) {
+          log.warning("Couldn't set status of soundchain:", response);
+        }
+    });
+  }
+
   /*
    * Dossiers
    */
