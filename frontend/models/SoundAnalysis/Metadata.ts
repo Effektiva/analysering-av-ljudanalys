c2172db6d875b047be63e4833afb7236937e1fb4
Rasmus Wallin <raswa151@student.liu.se>
2023-05-07 15:26:07 +0000

    removed dossiers list

--- frontend/models/SoundAnalysis/Metadata.ts
+++ frontend/models/SoundAnalysis/Metadata.ts
@@ -2,12 +2,10 @@ import Dossier from "../General/Dossier";
 
 class Metadata {
   fileName: string;
-  belongingDossiers: Array<Dossier>;
   fileFormat: string;
 
-  constructor(name: string, belongingDossiers: Array<Dossier>) {
+  constructor(name: string) {
     this.fileName = name.split(".")[0];
-    this.belongingDossiers = belongingDossiers;
     this.fileFormat = name.split(".")[1];
   }
 
@@ -21,10 +19,6 @@ class Metadata {
 
   public getDate(): string {
     return this.fileName.replace(/(\d{4})-(\d{2})-(\d{2})_(\d{2})(\d{2})_(\d{2})(\d{2})/, "$3-$2-$1");
-  }
-
-  public getBelongingDossiers(): Array<Dossier> {
-    return this.belongingDossiers;
   }
 }
 
